7e803ab8646d31e5d485610ce7f1267767732876
Rv072 <94162846+Rv072@users.noreply.github.com>
2022-05-28 15:00:25 +0530

    Render the Top picks movies using Reactjs

---
+++ main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Moviefetch from './Moviefetch'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+
+  <Moviefetch />
+
+)
